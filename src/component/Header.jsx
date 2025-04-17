706620888c2c78dba4e798b319e7f0a9c826c29e
Tanishk31 <tanishkchaudhary3108@gmail.com>
2025-04-17 21:42:14 +0530

    final changes_1

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,18 +8,8 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
-        <h1>
-          <a href="/" className="logo">
-            <img
-              src="/images/logo.svg"
-              width={40}
-              height={40}
-              alt="Ritika Pandit"
-            />
-          </a>
-        </h1>
-
-        <div className="relative md:justify-self-center">
+        <div></div>
+        <div className="relative flex justify-center">
           <button 
             className="menu-btn md:hidden"
             onClick={() => setNavOpen(prev => !prev)} // Toggle between open/close
